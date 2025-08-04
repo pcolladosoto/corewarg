4a3d5646c8027aacbfcdab1da81d40ce90eabff5
Pablo Collado Soto <pcolladosoto@gmx.com>
2025-08-04 19:48:00 +0200

    feat: add initial (dummy) parser

---
+++ parser/parser.go
@@ -0,0 +1,2 @@
+//go:generate goyacc -o icws94_ygen.go -p "corewar" icws94.y
+package parser
---
+++ parser/parser_test.go
@@ -0,0 +1,47 @@
+package parser
+
+import (
+	"log/slog"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/pcolladosoto/corewarg/lexer"
+)
+
+func init() {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelInfo,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			// Remove time.
+			if a.Key == slog.TimeKey && len(groups) == 0 {
+				return slog.Attr{}
+			}
+
+			// Remove the directory from the source's filename.
+			if a.Key == slog.SourceKey {
+				source := a.Value.Any().(*slog.Source)
+				source.File = filepath.Base(source.File)
+			}
+			return a
+		},
+	}))
+	slog.SetDefault(logger)
+}
+
+// Remember to run 'gp generate' beforehand!
+func TestParserSimple(t *testing.T) {
+	tests := []string{"5 ADD 4\n", "5 ADD 4"}
+	for _, test := range tests {
+		slog.Info("test", "rc", corewarParse(&corewarLex{l: lexer.Lex("parseTest", test)}))
+	}
+}
+
+func TestParserError(t *testing.T) {
+	tests := []string{"5 WRONG 4\n", "5 WRONG 4"}
+	for _, test := range tests {
+		slog.Info("test", "rc", corewarParse(&corewarLex{l: lexer.Lex("parseTest", test)}))
+	}
+
+}
